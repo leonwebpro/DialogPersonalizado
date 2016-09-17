7a9aac920c45e99cf741cb02daac5bf34eff544b
leonwebpro <leonwebpro@gmail.com>
2016-09-18 00:23:55 +0200

    commit git local

--- app/src/main/java/com/example/rober/alertdialogcustom/MainActivity.java
+++ app/src/main/java/com/example/rober/alertdialogcustom/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+// probando Git local
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
